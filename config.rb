d8cb2f9460da3ca6248d9aed429e8e94b7f29e99
Eric Bidelman <ebidel@gmail.com>
2012-04-21 17:06:54 -0700

    sass dir to scss

--- config.rb
+++ config.rb
@@ -3,7 +3,7 @@
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "theme/css"
-sass_dir = "theme/sass"
+sass_dir = "theme/scss"
 images_dir = "images"
 javascripts_dir = "js"
 
