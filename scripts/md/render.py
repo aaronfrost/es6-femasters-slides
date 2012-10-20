9c6a2933f11a180505d5f33c772828ed13a0a742
Eric Bidelman <ebidel@gmail.com>
2012-10-20 16:51:08 +0900

    remove print

--- scripts/md/render.py
+++ scripts/md/render.py
@@ -50,7 +50,6 @@ def postprocess_html(html, metadata):
   """Returns processed HTML to fit into the slide template format."""
   if metadata.get('build_lists') and metadata['build_lists'] == 'true':
     html = html.replace('<ul>', '<ul class="build">')
-    print html
   return html
 
 if __name__ == '__main__':
